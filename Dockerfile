0059e461b3e33e883280d21235ae3b5549b2e0c3
Jessy <Jessy.Renard@insa-rennes.fr>
2024-01-22 18:29:26 +0100

    Changement tests d'intégration avec images de base

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM rust:latest
 WORKDIR /app
 RUN git clone https://github.com/JessyQLOG/moseiik.git
-RUN cd /app/moseiik
+
 RUN cargo build --manifest-path=/app/moseiik/Cargo.toml --release
 RUN cargo test --manifest-path=/app/moseiik/Cargo.toml --release --
--- tests/temp.rs
+++ tests/temp.rs
@@ -9,18 +9,18 @@ mod tests {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     fn test_x86() {
         let o = Options{
-            image:String::from("assets/kit.jpeg"),
+            image:String::from("assets/target-small.png"),
             output:String::from("assets/result-x86.png"),
-            tiles:String::from("assets/images"),
+            tiles:String::from("assets/tiles-small"),
             scaling: 1,
-            tile_size: 25,
+            tile_size: 5,
             remove_used:false,
             verbose:true,
             simd:true,
             num_thread:1,
         };
         compute_mosaic(o);
-        let img1 = ImageReader::open("./assets/ground-truth-kit-x86-1.png").unwrap().decode().unwrap().to_rgb8();
+        let img1 = ImageReader::open("./assets/target-small.png").unwrap().decode().unwrap().to_rgb8();
         let img2 = ImageReader::open("./assets/result-x86.png").unwrap().decode().unwrap().to_rgb8();
         assert_eq!(img1, img2);
     }
@@ -49,18 +49,18 @@ mod tests {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     fn test_generic() {
         let o = Options{
-            image:String::from("assets/kit.jpeg"),
+            image:String::from("assets/target-small.png"),
             output:String::from("assets/result-generic.png"),
-            tiles:String::from("assets/images"),
+            tiles:String::from("assets/tiles-small"),
             scaling: 1,
-            tile_size: 25,
+            tile_size: 5,
             remove_used:false,
             verbose:true,
             simd:false,
             num_thread:1,
         };
         compute_mosaic(o);
-        let img1 = ImageReader::open("./assets/ground-truth-kit-x86-1.png").unwrap().decode().unwrap().to_rgb8();
+        let img1 = ImageReader::open("./assets/target-small.png").unwrap().decode().unwrap().to_rgb8();
         let img2 = ImageReader::open("./assets/result-generic.png").unwrap().decode().unwrap().to_rgb8();
         assert_eq!(img1, img2);
     }
